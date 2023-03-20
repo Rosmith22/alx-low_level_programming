9c46fa48b8b3da6ca23710a1f4bc1a28998dc4b0
Rosmith22 <smithrukayat1@gmail.com>
2023-03-20 05:47:24 -0700

    c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-	char MyStatement[] = "Programming is like building a multilingual puzzle";
+	char MyStatement[] = "Programming is like building a multilingual puzzle"
+		;
 	puts(MyStatement);
+
 	return (0);
 }
