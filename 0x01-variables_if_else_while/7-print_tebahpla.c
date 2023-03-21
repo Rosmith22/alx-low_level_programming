d7e41aa39f33164e969a413160e2e480ee387615
Rosmith22 <smithrukayat1@gmail.com>
2023-03-20 21:02:08 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int a;
+for (a = 122; a >= 97; a--)
+{
+	putchar(a);
+}
+putchar(10);
+return (0);
+}
