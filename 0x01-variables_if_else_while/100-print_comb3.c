0ad96f4e883d6119629b24e05218cb051292db71
Rosmith22 <smithrukayat1@gmail.com>
2023-03-20 21:44:16 -0700

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int a, b;
+for (a = '0'; a <= '9'; a++)
+{
+	for (b = a + 1; b <= '9'; b++)
+	{
+		putchar(a);
+		putchar(b);
+		if (a != '8' || b != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
