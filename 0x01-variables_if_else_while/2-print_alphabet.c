94400a873d68c1c7d89bbc0304cee280782d269a
Rosmith22 <smithrukayat1@gmail.com>
2023-03-20 20:26:20 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+  * main - Entry point
+  * Return: Always 1 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int a;
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
