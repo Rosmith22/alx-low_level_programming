8eb93abcec350d086e4bedaccee2d4bdb003b7aa
Rosmith22 <smithrukayat1@gmail.com>
2023-03-23 17:39:39 -0700

    uppercase checker

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper -  tells if a character is uppercase or not
+ * @c : letter to test
+ * Return: 1 if an uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
