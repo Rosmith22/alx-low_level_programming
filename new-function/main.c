cb888720578515de4f4a6e47cc01691ddffdbc32
Rosmith22 <smithrukayat1@gmail.com>
2023-03-27 15:12:37 -0700

    point to 98

---
+++ new-function/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*include your hader file below this*/
+
+/**
+ * function with no return and no parameter eg. void print_hi()
+ * function with return value eg. int random()
+ * function with return and parameter int random(int how_many_numbers)
+ * types of loop
+ * for loop
+ * while loop
+ * do while
+ */
+int main() {
+    int value;
+    printf("Enter how many numbers you need random of: ");
+    scanf("%d",&value);
+ random(value,1);
+
+    return 0;
+} 
---
+++ new-function/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H /*your define statement here -->*/
+#define MAIN_H /*your define statement here -->*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/* list of your functions prototype below this */
+
+#endif
